53368eca0e18c13b059ab75ae70c0d1e512cdab8
misaelzazueta <ingmisaelzazueta@gmail.com>
2020-09-22 21:35:25 -0600

    Ready for Jetson

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -1,9 +1,15 @@
 # example of zoom image augmentation
 from numpy import expand_dims
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import ImageDataGenerator
-from cv2 import *
+from keras_preprocessing.image import load_img
+from keras_preprocessing.image import img_to_array
+from keras_preprocessing.image import image_data_generator
+from cv2 import VideoCapture
+from cv2 import namedWindow
+from cv2 import imshow
+from cv2 import waitKey
+from cv2 import destroyAllWindows
+from cv2 import imwrite
+from cv2 import CAP_GSTREAMER
 import os
 
 print("Nombre: ")
@@ -13,19 +19,19 @@ directory = name
 parent_dir = "Photos"
 path = os.path.join(parent_dir, directory)
 os.mkdir(path)
-dir = parent_dir + "\\" + name
+dir = parent_dir + "/" + name
 img_n = name + ".png"
 
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("test")
+cam = VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=rotate-180 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink' , CAP_GSTREAMER)
+namedWindow("test")
 img_counter = 0
 while True:
     ret, frame = cam.read()
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("test", frame)
-    k = cv2.waitKey(1)
+    imshow("test", frame)
+    k = waitKey(1)
     if k%256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
@@ -33,12 +39,12 @@ while True:
     elif k%256 == 32:
         # SPACE pressed
         img_name = img_n.format(img_counter)
-        cv2.imwrite(img_name, frame)
+        imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
         break
 cam.release()
-cv2.destroyAllWindows()
+destroyAllWindows()
 
 # load the image
 img = load_img(img_n)
@@ -47,7 +53,7 @@ data = img_to_array(img)
 # expand dimension to one sample
 samples = expand_dims(data, 0)
 # create image data augmentation generator
-datagen = ImageDataGenerator(zoom_range=[0.5,1.0])
+datagen = image_data_generator.ImageDataGenerator(zoom_range=[0.5,1.0])
 # prepare iterator
 it = datagen.flow(samples, batch_size=1, save_to_dir= dir)
 # create image data augmentation generator
@@ -56,7 +62,7 @@ for i in range(10):
 	batch = it.next()
 	# convert to unsigned integers for viewing
 	image = batch[0].astype('uint8')
-datagen = ImageDataGenerator(rotation_range=90)
+datagen = image_data_generator.ImageDataGenerator(rotation_range=90)
 # prepare iterator
 it = datagen.flow(samples, batch_size=1, save_to_dir= dir)
 # create image data augmentation generator
@@ -65,7 +71,7 @@ for i in range(10):
 	batch = it.next()
 	# convert to unsigned integers for viewing
 	image = batch[0].astype('uint8')
-datagen = ImageDataGenerator(horizontal_flip=True)
+datagen = image_data_generator.ImageDataGenerator(horizontal_flip=True)
 # prepare iterator
 it = datagen.flow(samples, batch_size=1, save_to_dir= dir)
 # create image data augmentation generator
@@ -74,7 +80,7 @@ for i in range(10):
 	batch = it.next()
 	# convert to unsigned integers for viewing
 	image = batch[0].astype('uint8')
-datagen = ImageDataGenerator(shear_range= 30)
+datagen = image_data_generator.ImageDataGenerator(shear_range= 30)
 # prepare iterator
 it = datagen.flow(samples, batch_size=1, save_to_dir= dir)
 # generate samples and plot
